9865f9e8f5aee9bde9b0979821ef7a294150d937
isalazarjara@outlook.com <Ian Salazar>
2022-08-08 19:08:10 -0400

    deploy app

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://Naicriz.github.io/React-Rick-Morty-App/',
   plugins: [react()]
 })
